b79bb8271acb0d04340a94fea661772ff6f3f362
Orgil <orgil.u@gmail.com>
2021-10-29 10:50:05 +0800

    updated pairs plugin config

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,5 +1,6 @@
 local npairs = require('nvim-autopairs')
 local map = require"utils".map
+local cmp = require'cmp'
 
 npairs.setup{
   check_ts = true,
@@ -24,13 +25,14 @@ npairs.setup{
   ignored_next_char = "[%w%.%+%-%=%/%,]"
 }
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-})
+local cmp_pairs = require("nvim-autopairs.completion.cmp");
+
+cmp.event:on('confirm_done', cmp_pairs.on_confirm_done({
+      map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
+      auto_select = true, -- automatically select the first item
+      insert = false, -- use insert confirm behavior instead of replace
+      map_char = { -- modifies the function or method delimiter by filetypes
+        all = '(',
+        tex = '{'
+      }
+  }))
