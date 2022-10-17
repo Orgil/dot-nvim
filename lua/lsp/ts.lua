b7fd10351b77f17d4b19b5445bd497f1be3bf5a3
Orgil <orgil.u@gmail.com>
2022-10-17 14:34:58 +0800

    update cmp

--- lua/lsp/ts.lua
+++ lua/lsp/ts.lua
@@ -3,7 +3,7 @@ local buf_map = require "utils".buffer_map
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local on_attach = function(client, bufnr)
   client.server_capabilities.document_formatting = false
