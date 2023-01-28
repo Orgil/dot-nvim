6a229b210359012d4fde75c9ab27cce70bd3351d
Orgil <orgil.u@gmail.com>
2023-01-28 19:49:21 +0800

    golang setup

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -3,57 +3,18 @@ local helpers = require 'null-ls.helpers'
 local b = null_ls.builtins
 local buf_map = require 'utils'.buffer_map
 
--- local gdscript_formatter = {
---   method = null_ls.methods.FORMATTING,
---   filetypes = { 'gdscript', 'gd' },
---   generator = helpers.formatter_factory({
---     command = 'gdformat',
---     args = { '$FILENAME' },
---     to_stdin = false,
---     from_stderr = false,
---     to_temp_file = true
---   })
--- }
-
--- local gdscript_linter = {
---   method = null_ls.methods.DIAGNOSTICS,
---   filetypes = { 'gdscript', 'gd' },
---   generator = helpers.generator_factory({
---     command = 'gdlint',
---     args = { '$FILENAME' },
---     to_stdin = true,
---     from_stderr = true,
---     to_temp_file = true,
---     format = "line",
---     on_output = helpers.diagnostics.from_patterns({
---       {
---         pattern = [[:(%d+): ([%w-/]+): (.*)]],
---         groups = { "row", "severity", "message" },
---       }
---     })
---   })
--- }
-
 local sources = {
-  -- gdscript_linter,
-  -- gdscript_formatter,
-  -- b.diagnostics.eslint.with({
-  --     condition = function(utils)
-  --       return utils.root_has_file(".eslintrc")
-  --     end
-  --   }),
-  -- b.formatting.prettier,
-  -- b.code_actions.eslint.with({
-  --     condition = function(utils)
-  --       return utils.root_has_file(".eslintrc")
-  --     end
-  --   }),
-  -- b.code_actions.eslint,
   b.completion.vsnip,
+  b.code_actions.gomodifytags,
   b.code_actions.gitsigns,
   b.code_actions.refactoring,
-  b.diagnostics.stylelint
-  -- b.diagnostics.yamllint
+  b.diagnostics.stylelint,
+  b.diagnostics.golangci_lint,
+  b.diagnostics.gdlint,
+  b.formatting.fixjson,
+  b.formatting.fixjson,
+  b.formatting.gdformat,
+  b.formatting.gofmt
 }
 
 local on_attach = function(client, bufnr)
